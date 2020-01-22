20e1740726533acb27a8db829c1ac7422a1cdbde
Mahesh Maan <mahesh.maan@greyb.com>
2020-01-22 11:34:17 +0530

    Remove the unused processor.js and merge assignee-cleaner.js with stats.js

--- core/stats.js
+++ core/stats.js
@@ -1,6 +1,36 @@
 'use strict'
 
-let cleaner = require('./assignee-cleaner.js');
+let genericWords = 'l p l m n v oy sa kabushiki kaisha limited publ inc incorporated corp corporation co ltd llc nv gmbh services ag bv pvt pte pty electronics licensing holding holdings technologies products beijing and patent'.split(' ');
+
+function clean (assigneeName) {
+	assigneeName = assigneeName.trim();
+	let arr = assigneeName.split(/[\W+]/g).filter(e => e.trim());
+	while (arr.length > 1) {
+		let lastTerm = arr[arr.length-1].toLowerCase();
+		if (genericWords.includes(lastTerm)) {
+			arr.pop();
+		} else {
+			let str = arr.join(' ');
+			while (str.length > 60) {
+				str = str.replace(/\W+\w+$/, '');
+			}
+			return str;
+		}
+	}
+	let str = arr.join(' ');
+	while (str.length > 60) {
+		str = str.replace(/\W+\w+$/, '');
+	}
+	return str;
+}
+
+function isUniversity (assigneeName) {
+	if (assigneeName.match(/\b(univ|university|inst|institute)\b/i)) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 function getOScore(docs) {
 	let O_score = parseInt((docs[0].distance/1.3)*100);
@@ -27,7 +57,7 @@ function getYearStats(docs) {
 
 function getAssigneeStats(docs) {
 	docs = docs.filter(e => typeof e.assignee == 'string');
-	let assignees = docs.map(e => e.assignee).map(e => cleaner.clean(e));
+	let assignees = docs.map(e => e.assignee).map(e => clean(e));
 	return _assigneeDist(assignees);
 
 	function _assigneeDist (arr, max=6) {
