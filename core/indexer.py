1054401c1e27b532a863ac3b2dcd74d71b654d7b
Mahesh Maan <mahesh.maan@greyb.com>
2020-07-28 19:33:45 +0000

    Document code in indexer.py; still WIP

--- core/indexer.py
+++ core/indexer.py
@@ -10,45 +10,130 @@ from config.config import INDEX_DIR
 
 class Indexer():
 
+    """
+    Loads and caches vector indexes of various types (Annoy, FAISS).
+    """
+    
     class __impl:
 
+        """
+        A singleton class to make sure that only one copy of this
+        class exists in the memory. This is important because its
+        instances caches data in memory and multiple copies may lead to
+        caching of redundant data leading to increased memory
+        consumption.
+        """
+        
         def __init__(self):
-            """Summary
+            """Initialize an Indexer class.
             """
-            self.dir = INDEX_DIR
+
+            # directory where indexes are stored
+            self._dir = INDEX_DIR
+
+            # enabling this will speed up the search but will consume
+            # more momory
             self._cache_enabled = True
+
             self._cache = {}
+            self._default_index_type = 'faiss'
 
-        def __getitem__(self, index_id, indexType='faiss'):
-
-            if self._in_cache(index_id, indexType):
-                return self._get_index_from_cache(index_id, indexType)
+        def __getitem__(self, index_id):
+            """Get an index with given name.
+            
+            Args:
+                index_id (str): The index's name.
+            
+            Returns:
+                Index: The Index object corresponding to the requested
+                    `index_id`.
+            """
+            return self.get_index(index_id)
+            
+        def get_index(self, index_id, index_type=self._default_index_type):
+            """Get the index with the given name and type.
+            
+            Args:
+                index_id (str): The index's name.
+                index_type (str, optional): The index's type, e.g.,
+                    'annoy' or 'faiss'.
+            
+            Returns:
+                Index: The Index object corresponding to the given
+                    name and index type.
+            """
+            if self._in_cache(index_id, index_type):
+                return self._get_index_from_cache(index_id, index_type)
             else:
-                index = self._get_index_from_disk(index_id, indexType)
+                index = self._get_index_from_disk(index_id, index_type)
                 return index
 
-        def _in_cache(self, index_id, indexType):
-            cached = self._cache.get([(index_id, indexType)])
-            return bool(cached)
+        def _in_cache(self, index_id, index_type):
+            """Check whether an index is cached in the memory.
+            
+            Args:
+                index_id (str): The index's name.
+                index_type (str): The index's type.
+            
+            Returns:
+                bool: True if the index is available in the cache, False
+                    otherwise.
+            """
+            key = (index_id, index_type)
+            return key in self._cache
 
-        def _get_index_from_cache(self, index_id, indexType):
-            cached = self._cache.get([(index_id, indexType)])
+        def _get_index_from_cache(self, index_id, index_type):
+            """Return the specified Index object from the cache.
+
+            Args:
+                index_id (str): The index's name.
+                index_type (str): The index's type.
+            
+            Returns:
+                Index: The Index object corresponding to the given
+                    name and index type.
+            """
+            key = (index_id, index_type)
+            cached = self._cache.get(key)
             return cached
 
-        def _add_index_to_cache(self, index_id, indexType, index):
-            self._cached[(index_id, indexType)] = index
-            return True
+        def _add_index_to_cache(self, index_id, index_type, index):
+            """Add the index to in-memory cache.
+            
+            Args:
+                index_id (str): The Index's name.
+                index_type (str): The Index's type.
+                index (Index): An object of the class Index.
+            
+            Returns:
+                bool: True if caching successful, False otherwise.
+            """
+            key = (index_id, index_type)
+            self._cache[key] = index
+            return key in self._cache
 
-        def _get_index_from_disk(self, index_id, indexType):
-            if indexType == 'faiss':
-                index = IndexFaiss(index_id, self.dir)
-            elif indexType == 'annoy':
-                index = IndexAnnoy(index_id, self.dir)
+        def _get_index_from_disk(self, index_id, index_type):
+            """Return the specified Index object by loading it from the
+                disk.
+
+            Args:
+                index_id (str): The index's name.
+                index_type (str): The index's type.
+            
+            Returns:
+                Index: The Index object corresponding to the given
+                    name and index type.
+            """
+            if index_type == 'faiss':
+                index = IndexFaiss(index_id, self._dir)
+            elif index_type == 'annoy':
+                index = IndexAnnoy(index_id, self._dir)
             else:
                 raise Exception('Invalid index type.')
 
+            # Cache for later use
             if self._cache_enabled:
-                self._add_to_cache(index_id, indexType, index)
+                self._add_to_cache(index_id, index_type, index)
 
             return index
 
@@ -62,6 +147,64 @@ class Indexer():
 
     def __getitem__(self, key):
         return self.__instance.__getitem__(key)
+
+class IndexAnnoy():
+
+    def __init__(self, index_id, folder):
+        self.index_id = index_id
+        self.folder = folder
+        self.index_file_path = f"{self.folder}{self.index_id}.ann"
+        self.labels_file_path = f"{self.index_file_path}.items.json"
+        self.dims = None
+
+    def __getitem__(self, index_id):
+        pass
+
+    def create(self, vectors, labels=None):
+        nvecs = vectors.shape[0]
+        self.ndims = vectors.shape[1]
+        index = annoy.AnnoyIndex(self.ndims, 'angular')
+        for i in range(nvecs):
+            index.add_item(i, vectors[i])
+        ntrees = 20
+        index.build(ntrees)
+        index.save(self.index_file_path)
+        if labels is None:
+            labels = list(range(nvecs))
+        with open(self.labels_file_path) as f:
+            f.write(json.dumps(labels))
+        return True
+
+    def get_vectors(self):
+        index = annoy.AnnoyIndex(self.ndims, 'angular')
+        index.load(self.index_file_path)
+        vecs = np.empty((index.get_n_items(), self.ndims), 'float32')
+        for j in range(nvecs):
+            vecs[j] = index.get_item_vector(j)
+        return vecs
+
+    def get_labels(self, idxs=None):
+        with open(f"{self.folder}G06N.ann.items.json") as f:
+            data = json.load(f)
+        labels = np.array(data)
+        return labels
+
+    def find_similar(self, query_vectors, n):
+        index.load(f"{self.index_id}.ann")
+        nearest_neighbours = n
+        indices = index.get_nns_by_vector(
+            query_vectors, nearest_neighbours, include_distances=False)
+        return indices
+
+    def find_similar_with_dist(self, query_vectors, n):
+        index.load(f"{self.index_id}.ann")
+        nearest_neighbours = n
+        distances = index.get_nns_by_vector(
+            query_vectors, nearest_neighbours, include_distances=True)[1]
+        return distances
+
+    def get_n_items():
+        return index.get_n_items()
 
 
 class IndexFaiss():
@@ -158,62 +301,3 @@ class IndexFaiss():
 
     def get_n_items():
         pass
-
-
-class IndexAnnoy():
-
-    def __init__(self, index_id, folder):
-        self.index_id = index_id
-        self.folder = folder
-        self.index_file_path = f"{self.folder}{self.index_id}.ann"
-        self.labels_file_path = f"{self.index_file_path}.items.json"
-        self.dims = None
-
-    def __getitem__(self, index_id):
-        pass
-
-    def create(self, vectors, labels=None):
-        nvecs = vectors.shape[0]
-        self.ndims = vectors.shape[1]
-        index = annoy.AnnoyIndex(self.ndims, 'angular')
-        for i in range(nvecs):
-            index.add_item(i, vectors[i])
-        ntrees = 20
-        index.build(ntrees)
-        index.save(self.index_file_path)
-        if labels is None:
-        	labels = list(range(nvecs))
-        with open(self.labels_file_path) as f:
-        	f.write(json.dumps(labels))
-        return True
-
-    def get_vectors(self):
-        index = annoy.AnnoyIndex(self.ndims, 'angular')
-        index.load(self.index_file_path)
-        vecs = np.empty((index.get_n_items(), self.ndims), 'float32')
-        for j in range(nvecs):
-            vecs[j] = index.get_item_vector(j)
-        return vecs
-
-    def get_labels(self, idxs=None):
-        with open(f"{self.folder}G06N.ann.items.json") as f:
-            data = json.load(f)
-        labels = np.array(data)
-        return labels
-
-    def find_similar(self, query_vectors, n):
-        index.load(f"{self.index_id}.ann")
-        nearest_neighbours = n
-        indices = index.get_nns_by_vector(
-            query_vectors, nearest_neighbours, include_distances=False)
-        return indices
-
-    def find_similar_with_dist(self, query_vectors, n):
-        index.load(f"{self.index_id}.ann")
-        nearest_neighbours = n
-        distances = index.get_nns_by_vector(
-            query_vectors, nearest_neighbours, include_distances=True)[1]
-        return distances
-
-    def get_n_items():
-        return index.get_n_items()
