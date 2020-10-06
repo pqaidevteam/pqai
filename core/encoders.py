b91ddb409b336ae5916a3897c03eda5cbd06357c
Mahesh Maan <mahesh.maan@greyb.com>
2020-10-06 18:18:38 +0000

    Update BagOfEntitiesEncoder and Add EmbeddingMatrix to encoders

--- core/encoders.py
+++ core/encoders.py
@@ -1,6 +1,7 @@
 from config.config import models_dir
 from core import utils
 import re
+import numpy as np
 
 class Encoder:
 
@@ -13,36 +14,26 @@ class Encoder:
 		self._input_validation_fn = None
 		self._name = 'Encoder'
 
-	@property
-	def encoder_fn(self):
-		return self._encoder_fn
-	
-	@encoder_fn.setter
-	def encoder_fn(self, fn):
+	def set_encoding_fn(self, fn):
 		self._encoder_fn = fn
 
-	@property
-	def input_validation_fn(self):
-		return self._input_validation_fn
-	
-	@input_validation_fn.setter
-	def input_validation_fn(self, fn):
+	def set_input_validation_fn(self, fn):
 		self._input_validation_fn = fn
 
 	def encode(self, item):
-		self._can_encode(item)
+		self._raise_exception_if_incompatible(item)
 		return self._encoder_fn(item)
 
 	def encode_many(self, items):
 		return [self.encode(item) for item in items]
 
-	def _can_encode(self, data):
+	def _raise_exception_if_incompatible(self, data):
 		if not callable(self._encoder_fn):
 			self._raise_invalid_encoder_fn_exception()
-		if not self.is_valid_input(data):
+		if not self.can_encode(data):
 			self._raise_invalid_input_data_exception()
 
-	def is_valid_input(self, data):
+	def can_encode(self, data):
 		is_valid = self._input_validation_fn
 		return False if callable(is_valid) and not is_valid(data) else True
 
@@ -55,45 +46,41 @@ class Encoder:
 		raise Exception(msg)
 
 
-class TokenSequenceEncoder(Encoder):
-	
-	def __init__(self, fn=None):
-		super().__init__(fn)
-		self._name = 'TokenSequenceEncoder'
-		self._input_validation_fn = lambda x: isinstance(x, str)
-
-
-class BagOfWordsEncoder(Encoder):
-	
-	def __init__(self, fn=None):
-		super().__init__(fn)
-		self._name = 'BagOfTokensEncoder'
-		self._input_validation_fn = lambda x: isinstance(x, str)
-
-
 class BagOfEntitiesEncoder(Encoder):
 
+	@classmethod
+	def from_vocab_file(self, filepath):
+		encoder = BagOfEntitiesEncoder()
+		encoder._vocab_file = filepath
+		return encoder
+
 	def __init__(self):
-		super().__init__(self._get_entities)
+		super().__init__()
 		self._name = 'BagOfEntitiesEncoder'
+		self.set_encoding_fn(self._get_entities)
+		self.set_input_validation_fn(lambda x: isinstance(x, str))
+		self._vocab_file = None
 		self._vocab = None
-		self._lut = None
+		self._lookup_table = None
 		self._no_casing = True
 		self._maxlen = 3
 		self._separator = ' '
-		self._vocab_file = models_dir + 'entities.txt'
 		self._sent_tokenizer = utils.get_sentences
-		self._input_validation_fn = lambda x: isinstance(x, str)
 
-	def _load_vocab_from_txt_file(self):
+	def set_maxlen(self, n):
+		self._maxlen = n
+
+	def set_separator(self, sep):
+		self._separator = sep
+
+	def _load_vocab(self):
 		with open(self._vocab_file) as fp:
 			self._vocab = fp.read().strip().splitlines()
-			self._lut = set(self._vocab)
+			self._lookup_table = set(self._vocab)
 
 	def _load_vocab_if_unloaded(self):
-		if isinstance(self._vocab, list):
-			return
-		self._load_vocab_from_txt_file()
+		if not isinstance(self._vocab, list):
+			self._load_vocab()
 
 	def _get_entities(self, text):
 		self._load_vocab_if_unloaded()
@@ -104,7 +91,7 @@ class BagOfEntitiesEncoder(Encoder):
 
 	def _get_entities_from_sentence(self, sentence):
 		candidates = self._get_candidate_entities(sentence)
-		return [c for c in candidates if c in self._lut]
+		return [c for c in candidates if c in self._lookup_table]
 
 	def _get_candidate_entities(self, sent):
 		candidates = set()
@@ -123,7 +110,79 @@ class BagOfEntitiesEncoder(Encoder):
 
 	def _tokenize(self, text):
 		text = text.lower() if self._no_casing else text
-		pattern = r'(\w+|\W+)'
+		pattern = r'([\w\-]+|\W+)'
 		matches = re.findall(pattern, text)
 		tokens = [m for m in matches if m.strip()]
 		return tokens
+
+
+class EmbeddingMatrix():
+
+	def __init__(self, vocab=None, vectors=None):
+		self._items = None
+		self._vectors = None
+		self._lookup = None
+		self._unit_vectors = None
+
+	@property
+	def dims(self):
+		if self._vectors is None:
+			return None
+		vec = self._vectors[0]
+		return len(vec)
+
+	def __getitem__(self, key):
+		idx = self._lookup[key]
+		return self._vectors[idx]
+
+	def similar_to(self, item, n=10):
+		item_vector = self[item]
+		cosine_prods = np.dot(item_vector, self._unit_vectors.T)
+		idxs = np.argsort(cosine_prods)[:n][::-1]
+		return [self._items[i] for i in idxs]
+
+	@classmethod
+	def from_tsv(self, filepath):
+		emb = EmbeddingMatrix()
+		emb._load_from_tsv_file(filepath)
+		return emb
+
+	def _load_from_tsv_file(self, filepath):
+		pairs = self._parse_tsv_file(filepath)
+		self._items = [word for word, _ in pairs]
+		self._vectors = np.array([vector for _, vector in pairs])
+		self._lookup = {w:i for i,w in enumerate(self._items)}
+		self._unit_vectors = self._normalize_vectors(self._vectors)
+
+	def _parse_tsv_file(self, filepath):
+		with open(filepath) as file:
+			lines = (l for l in file if l.strip())
+			pairs = [self._parse_tsv_line(l) for l in lines]
+		return pairs
+
+	def _parse_tsv_line(self, line):
+		[word, *vector] = line.strip().split('\t')
+		vector = [float(val) for val in vector]
+		return word, vector
+
+	def _normalize_vectors(self, M):
+		epsilon = np.finfo(float).eps
+		norms = np.sqrt((M*M).sum(axis=1, keepdims=True))
+		norms += epsilon	# to avoid division by zero
+		return M / norms
+
+
+class TokenSequenceEncoder(Encoder):
+	
+	def __init__(self, fn=None):
+		super().__init__(fn)
+		self._name = 'TokenSequenceEncoder'
+		self._input_validation_fn = lambda x: isinstance(x, str)
+
+
+class BagOfWordsEncoder(Encoder):
+	
+	def __init__(self, fn=None):
+		super().__init__(fn)
+		self._name = 'BagOfTokensEncoder'
+		self._input_validation_fn = lambda x: isinstance(x, str)
--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -2,11 +2,13 @@ import unittest
 
 import sys
 from pathlib import Path
+test_dir = str(Path(__file__).parent.resolve())
 BASE_DIR = Path(__file__).parent.parent
 sys.path.append(str(BASE_DIR.resolve()))
 
 from core.encoders import Encoder
 from core.encoders import BagOfEntitiesEncoder
+from core.encoders import EmbeddingMatrix
 
 class TestEncoderClass(unittest.TestCase):
 
@@ -22,7 +24,7 @@ class TestEncoderClass(unittest.TestCase):
 
 	def test_can_set_encoding_fn_afterwards(self):
 		encoder = Encoder()
-		encoder.encoder_fn = list
+		encoder.set_encoding_fn(list)
 		data = 'hello'
 		expected = ['h', 'e', 'l', 'l', 'o']
 		actual = encoder.encode(data)
@@ -35,47 +37,85 @@ class TestEncoderClass(unittest.TestCase):
 		self.assertRaises(Exception, encode)
 
 	def test_input_validation(self):
-		validation_fn = lambda x: isinstance(x, str)
+		fn = lambda x: isinstance(x, str)
 		encoder = Encoder(list)
-		encoder.input_validation_fn = validation_fn
-		a = encoder.is_valid_input('hello')
-		b = encoder.is_valid_input([0, 2, 3])
+		encoder.set_input_validation_fn(fn)
+		a = encoder.can_encode('hello')
+		b = encoder.can_encode([0, 2, 3])
 		self.assertEqual((True, False), (a, b))
 
-	def test_can_encode_multiple_inputs_in_one_go(self):
+	def test_can_encode_array_of_items(self):
 		encoder = Encoder(list)
 		inputs = ['hi', 'hello']
 		expected = [list('hi'), list('hello')]
 		actual = encoder.encode_many(inputs)
 		self.assertEqual(expected, actual)
 
+
 class TestBagOfEntitiesEncoderClass(unittest.TestCase):
 
 	def setUp(self):
-		self.boe_encoder = BagOfEntitiesEncoder()
+		file = f'{test_dir}/test_entities.vocab'
+		self.boe_encoder = BagOfEntitiesEncoder.from_vocab_file(file)
+
+	def test_can_encode_string(self):
+		res = self.boe_encoder.can_encode('This is a sample text.')
+		self.assertTrue(res)
+
+	def test_can_encode_array(self):
+		res = self.boe_encoder.can_encode([1, 2, 3])
+		self.assertFalse(res)
 
 	def test_extract_entities_from_one_sentence(self):
 		sent = 'base station and mobile station'
-		expected = set([ 'base', 'station', 'mobile',
-			'base station', 'mobile station'])
+		expected = set([ 'base station', 'mobile station'])
 		actual = self.boe_encoder.encode(sent)
 		self.assertEqual(expected, actual)
 
 	def test_extract_entities_from_two_sentences(self):
 		sents = 'Base station and mobile. Station is there.'
-		expected = set(['base', 'station', 'mobile', 'base station'])
+		expected = set(['base station'])
 		actual = self.boe_encoder.encode(sents)
 		self.assertEqual(expected, actual)
 
-	def test_with_trivial_string(self):
+	def test_with_string_devoid_of_entities(self):
 		trivial_string = 'the of and'
 		actual = self.boe_encoder.encode(trivial_string)
 		self.assertEqual(set(), actual)
 
-	def test_with_null_string(self):
+	def test_with_degenerate_string(self):
 		null_string = ''
 		actual = self.boe_encoder.encode(null_string)
 		self.assertEqual(set(), actual)
+
+	def test_captures_hyphenated_entities(self):
+		string = "This is an X-ray machine."
+		expected = set(['x-ray'])
+		actual = self.boe_encoder.encode(string)
+		self.assertEqual(expected, actual)
+
+
+class TestEmbeddingMatrixClass(unittest.TestCase):
+
+	def setUp(self):
+		file = f'{test_dir}/test_embs.tsv'
+		self.emb = EmbeddingMatrix.from_tsv(file)
+
+	def test_get_embedding_by_word(self):
+		item = 'base'
+		expected_emb = [1.0, 0.0]
+		actual_emb = list(self.emb[item])
+		self.assertEqual(expected_emb, actual_emb)
+
+	def test_can_get_dimensions(self):
+		n_dim = self.emb.dims
+		self.assertEqual(2, n_dim)
+
+	def test_similar_items(self):
+		item = 'station'
+		similars = self.emb.similar_to(item)
+		most_similar = similars[1] # 0 is the identical item
+		self.assertEqual('stations', most_similar)
 
 if __name__ == '__main__':
     unittest.main()
