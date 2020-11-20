478f60dd3f6777e0aa51fbe3d205ea76f8b16876
Mahesh Maan <mahesh.maan@greyb.com>
2020-11-20 07:45:35 +0000

    Add sensible span extractor modules

---
+++ core/sensible_span_extractor.py
@@ -0,0 +1,252 @@
+import numpy as np
+import re
+import json
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Model
+import tensorflow.keras.backend as K
+
+from config.config import models_dir
+
+class SensibleSpanExtractor():
+
+	vocab_dict_file = os.path.join(models_dir, 'span_extractor_dictionary.json')
+	vocab_file = os.path.join(models_dir, 'span_extractor_vocab.json')
+	model_file = os.path.join(models_dir, 'span_extractor_model.hdf5')
+	embeddings_file = os.path.join(models_dir, 'span_extractor_vectors.txt')
+
+	def __init__(self):
+		self._model = None
+		self._dims = 16
+		self._vocab_size = 844
+		self._dict = {}
+		self._embs = {}
+		self._emb_matrix = np.empty((self._vocab_size, self._dims))
+		self._vocab_map = {}
+		self._punct_map = {
+			'!': '<exclm>',
+			'"': '<dinvc>',  
+			'#': '<hash>',  
+			'$': '<dlr>',
+			'%': '<pcnt>', 
+			'&': '<and>', 
+			"'": '<sinvc>', 
+			'(': '<lb>',  
+			')': '<rb>',  
+			'*': '<astk>',  
+			'+': '<plus>',  
+			',': '<coma>',  
+			'-': '<minus>',  
+			'.': '<fstp>',  
+			'/': '<fslsh>',  
+			':': '<cln>',  
+			';': '<scln>',  
+			'<': '<lt>',  
+			'=': '<eq>',  
+			'>': '<gt>',  
+			'?': '<qm>',  
+			'@': '<arte>',  
+			'[': '<lsb>',  
+			'\\': '<bslsh>',  
+			']': '<rsb>',  
+			'^': '<rtp>',  
+			'_': '<uscr>',  
+			'`': '<btck>',  
+			'{': '<lcb>',  
+			'|': '<pipe>',  
+			'}': '<rcb>',  
+			'~': '<tlde>',  
+			'“': 'sdinvc',  
+			'”': 'edinvc'
+		}
+		self.chars = 'abcdefghijklmnopqrstuvwxyz'
+		self.MIN_LEN = 5
+		self.MAX_LEN = 30
+
+		self._load_model()
+		self._load_dict()
+		self._load_embeddings()
+		self._load_embedding_matrix()
+		self._load_vocab_map()
+
+	def _load_model(self):
+		model = load_model(self.model_file)
+		self._model = Model(model.input[:2], model.layers[-3].output)
+
+	def _load_dict(self):
+		with open(self.vocab_dict_file, 'r') as file:
+			self._dict = json.load(file)
+
+	def _load_embeddings(self):
+		with open(self.embeddings_file, 'r') as file:
+			lines = file.read().strip().splitlines()
+		for line in lines:
+			token, *vector = line.strip().split()
+			self._embs[token] = np.array(vector, dtype='float32')
+		self._include_pad_unk_vectors()
+
+	def _include_pad_unk_vectors(self):
+		low = -0.00001
+		high = 0.00001
+		self._embs['<pad>'] = [np.random.uniform(low, high) 
+			for _ in range(self._dims)]
+		self._embs['<unk>'] = self._embs['<raw_unk>']
+		self._embs.pop('<raw_unk>')
+
+	def _load_embedding_matrix(self):
+		for word, i in self._dict.items():
+			self._emb_matrix[i] = self._embs[word]
+
+	def _load_vocab_map(self):
+		with open(self.vocab_file, 'r') as fd:
+			vocab = json.load(fd)
+			self._vocab_map = {word:True for word in vocab}
+
+	def extract_from(self, sentence):
+		candidates = self._encode_for_nn(sentence)
+		i = self._rank(candidates)[0]
+		return self._strip_punctuations(' '.join(candidates[0][i]))
+
+	def return_ranked(self, sentence):
+		candidates = self._encode_for_nn(sentence)
+		ns = self._rank(candidates)
+		return [self._strip_punctuations(' '.join(candidates[0][n])) for n in ns]
+
+	def _rank(self, candidates):
+		tokens, X_chargrams, X_word_vectors = candidates
+		pred = self._model.predict([X_word_vectors, X_chargrams]).flatten()
+		pred = K.softmax(pred)
+		return np.argsort(pred)[::-1]
+
+	def _encode_for_nn(self, sentence):
+		cased = self._tokenize(sentence, lower=False)
+		uncased = [t.lower() for t in cased]
+		limits = [self.MIN_LEN, self.MAX_LEN]
+		tokens = SubsequenceExtractor(cased).extract(*limits)
+		spans = SubsequenceExtractor(uncased).extract(*limits)
+		chargrams = [self._span2chargram(span) for span in spans]
+		word_vectors = [self._embed_words(span) for span in spans]
+		return tokens, np.array(chargrams), np.array(word_vectors)
+
+	def _tokenize(self, sentence, lower=True):
+		sentence = sentence.lower() if lower else sentence
+		tokens = re.findall(r'(\w+|\W+)', sentence)
+		tokens = [t for t in tokens if t.strip()]
+		return tokens
+
+	def _get_possible_span_lengths(self, n_tokens):
+		if n_tokens <= self.MIN_LEN:
+			return [n_tokens]
+		elif self.MIN_LEN < n_tokens <= self.MAX_LEN:
+			return list(range(self.MIN_LEN, n_tokens+1))
+		else: # n_tokens > self.MAX_LEN
+			return list(range(self.MIN_LEN, self.MAX_LEN))
+
+	def _span2chargram(self, span):
+		span_len = min(len(span), self.MAX_LEN)
+		span_chargram_unpadded = [self._word2chargram(span[i]) for i in range(span_len)]
+		span_chargram = self._padding_int_arr(span_chargram_unpadded)
+		return span_chargram
+
+	def _word2chargram(self, word):   
+		chargrams =[0]*20
+		char_list = list(word)
+		for i in range(min(len(char_list), 20)):
+			if char_list[i] in self.chars:
+				chargrams[i] = self.chars.index(char_list[i])+1
+		return chargrams
+
+	def _embed_words(self, span):
+		span_len = min(len(span), self.MAX_LEN)
+		span_int_array = self._to_int_array(span)
+		span_word_emb_unpadded = [self._emb_matrix[span_int_array[i]] for i in range(span_len)]
+		span_word_emb = self._padding_int_arr(span_word_emb_unpadded)
+		return span_word_emb
+
+	def _get_masked_tokens(self, sent_tokens):
+		masked_sent_tokens = []
+		for token in sent_tokens:
+			if self._is_number(token):
+				masked_sent_tokens.append('<num>')
+			elif self._is_alphanumeric(token, 'fast'):
+				masked_sent_tokens.append('<alphanum>')
+			elif token in self._punct_map:
+				masked_sent_tokens.append(self._punct_map[token])
+			elif token not in self._vocab_map:
+				masked_sent_tokens.append('<unk>')
+			else:
+				masked_sent_tokens.append(token)
+		return masked_sent_tokens
+
+	def _is_alphanumeric(self, token, mode='slow'):
+		if token.isalnum(): 
+			if not token.isalpha():
+				if mode == 'slow':
+					if is_number(token):
+						return False
+				return True 
+		return False
+
+	def _is_number(self, token):
+		return bool(re.search(r'^\d+(\.\d+)?$', token))
+
+	def _encode_tokens(self, tokens):
+		encoded_tokens = []
+		for token in tokens:
+			encoded_tokens.append(self._dict[token])
+		return encoded_tokens
+
+	def _to_int_array(self, sent_tokens):
+		masked_sent_tokens = self._get_masked_tokens(sent_tokens)
+		encoded_tokens = self._encode_tokens(masked_sent_tokens)
+		return encoded_tokens
+
+	def _get_pad_token(self, int_arr):
+		if len(int_arr[0]) == 16:
+			pad_token = self._emb_matrix[0]
+		else:
+			pad_token = self._word2chargram('0')
+		return pad_token
+
+	def _padding_int_arr(self, int_arr):
+		pad_token = self._get_pad_token(int_arr)
+		for j in range(self.MAX_LEN-len(int_arr)):
+			int_arr.append(pad_token)
+		int_arr = np.array(int_arr)
+		return int_arr
+
+	def _strip_punctuations(self, text):
+		patterns = [
+			r'\s([\!\%\)\,\.\:\;\?\]\}\”])',
+			r'([\"\#\$\(\@\[\\\{\“])\s',
+			r'\s([\'\*\+\-\/\<\=\>\^\_\`\|\~])\s']
+		for pattern in patterns:
+			text = re.sub(pattern, r'\1', text)
+		return text
+
+class SubsequenceExtractor():
+
+	def __init__(self, sequence):
+		self._seq = sequence
+		self._seqlen = len(sequence)
+
+	def extract(self, minlen, maxlen=None):
+		maxlen = minlen if maxlen is None else maxlen
+		subsequences = []
+		for L in self._possible_lengths(minlen, maxlen):
+			subsequences += self._get_subsequences_of_length(L)
+		return subsequences
+
+	def _possible_lengths(self, minlen, maxlen):
+		if self._seqlen <= minlen:
+			return [self._seqlen]
+		elif minlen < self._seqlen <= maxlen:
+			return list(range(minlen, self._seqlen+1))
+		else: # n_tokens > maxlen
+			return list(range(minlen, maxlen+1))
+
+	def _get_subsequences_of_length(self, L):
+		if L == 0:
+			return []
+		start_positions = range(self._seqlen-L+1)
+		return [self._seq[p:p+L] for p in start_positions]
---
+++ tests/test_sensible_span_extractor.py
@@ -0,0 +1,49 @@
+import unittest
+import sys
+from pathlib import Path
+TEST_DIR = str(Path(__file__).parent.resolve())
+BASE_DIR = str(Path(__file__).parent.parent.resolve())
+sys.path.append(BASE_DIR)
+
+from core.sensible_span_extractor import SensibleSpanExtractor, SubsequenceExtractor
+
+
+class TestSensibleSpanExtractor(unittest.TestCase):
+
+	def setUp(self):
+		self.extractor = SensibleSpanExtractor()
+
+	def test_generates_span_1(self):
+		sent = 'The present invention relates to an epitaxial group-III-nitride buffer layer structure on a heterosubstrate.'
+		span = 'group-III-nitride buffer layer structure'
+		self.assertSpan(sent, span)
+
+	def test_generates_span_2(self):
+		sent = 'Another object of the present invention is to provide a fire fighting drone.'
+		span = 'a fire fighting drone.'
+		self.assertSpan(sent, span)
+
+	def assertSpan(self, sent, span):
+		self.assertEqual(span, self.extractor.extract_from(sent))
+
+
+class TestSubsequenceExtractor(unittest.TestCase):
+
+	def setUp(self):
+		self.extractor = SubsequenceExtractor([0, 1, 2, 3])
+
+	def test_subsequences_of_zero_length(self):
+		subseqs = self.extractor.extract(0)
+		self.assertEqual(0, len(subseqs))
+
+	def test_subsequences_of_unit_length(self):
+		subseqs = self.extractor.extract(1)
+		self.assertEqual(4, len(subseqs))
+
+	def test_subsequences_between_lengths(self):
+		subseqs = self.extractor.extract(2, 3)
+		self.assertEqual(5, len(subseqs))
+
+
+if __name__ == '__main__':
+	unittest.main()
