6811a55fadf81063c232c372d78c248e17edd379
Mahesh Maan <mahesh.maan@greyb.com>
2021-04-04 21:30:38 +0530

    Move more configuration parameters to environment variables

--- config/config.py
+++ config/config.py
@@ -6,6 +6,7 @@ env_file = f'{base_dir}/.env'
 if os.path.isfile(env_file):
     from dotenv import load_dotenv
     load_dotenv(env_file)
+    print('Created environment from .env file.')
 
 if os.environ.get('TEST'):
     indexes_dir = f'{base_dir}/tests/test_indexes/'
@@ -23,13 +24,22 @@ mongo_dbname = os.environ['MONGO_DBNAME']
 mongo_pat_coll = os.environ['MONGO_PAT_COLL']
 mongo_npl_coll = os.environ['MONGO_NPL_COLL']
 
-index_selection_disabled = True
-reranker_active = True
-gpu_disabled = True
+index_selection_disabled = not bool(int(os.environ['SMART_INDEX_SELECTION']))
+if index_selection_disabled:
+    print('WARNING: Index selection is inactive. Search may be slow.')
+
+reranker_active = bool(int(os.environ['USE_RERANKER']))
+gpu_disabled = bool(int(os.environ['DISABLE_GPU']))
+if gpu_disabled:
+    print('Application has been configured to run without GPU.')
+else:
+    print('Application will use GPU if available.')
 
 extensions = []
-allow_outgoing_extension_requests = False
-allow_incoming_extension_requests = False
+allow_outgoing_extension_requests = bool(int(os.environ['OUTGOING_EXT']))
+allow_incoming_extension_requests = bool(int(os.environ['INCOMING_EXT']))
+if allow_incoming_extension_requests:
+    print('Server has been configured to accept extension requests.')
 
 port = int(os.environ['API_PORT'])
 PQAI_S3_BUCKET_NAME = os.environ['PQAI_S3_BUCKET_NAME']
