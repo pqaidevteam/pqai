946f6665aa146e0b96f10bcbafaf3189761c1d10
Mahesh Maan <mahesh.maan@greyb.com>
2021-04-04 15:51:25 +0530

    Move request authentication logic to auth.py from server.py where it belongs more

--- auth.py
+++ auth.py
@@ -6,3 +6,29 @@ def read_tokens():
         lines = f.read().strip().splitlines()
         tokens = [re.split(r'\s+', line)[0] for line in lines]
     return set(tokens)
+
+tokens = read_tokens()
+
+def validate_access(request):
+    route = request.base_url
+    if not is_behind_auth(route):
+        return True
+    token = extract_token(request)
+    return token in tokens
+
+def extract_token(req):
+    method = req.method
+    if method == 'GET':
+        return req.args.to_dict().get('token')
+    elif method == 'POST':
+        return req.json.get('token')
+    else:
+        return None
+
+def is_behind_auth(route):
+    if route.endswith(('.css', '.js', '.ico')) \
+        or '/drawings' in route \
+        or '/thumbnails' in route \
+        or '/docs' in route:
+        return False
+    return True
