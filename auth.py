5a75cd2a0db9c54ebc262b5b7f9a7da03800603a
Mahesh Maan <mahesh.maan@greyb.com>
2023-04-12 12:33:09 +0530

    Add API token usage counter

--- auth.py
+++ auth.py
@@ -1,6 +1,16 @@
 import os
 import re
+import logging
+from logging import FileHandler, Formatter
 from config.config import tokens_file
+
+logger = logging.getLogger('API-ACCESS')
+logger.setLevel(logging.DEBUG)
+
+fh = FileHandler('api-access.log')
+fh.setLevel(logging.INFO)
+fh.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(fh)
 
 TOKENS = set()
 
@@ -8,31 +18,38 @@ def read_tokens():
     global TOKENS
     if not os.path.isfile(tokens_file):
         return set()
-    with open(tokens_file, 'r') as f:
+    with open(tokens_file, "r", encoding="utf-8") as f:
         lines = f.read().strip().splitlines()
         tokens = [re.split(r'\s+', l)[0] for l in lines if l.strip()]
         TOKENS = TOKENS.union(tokens)
 
 def validate_access(request):
-    route = request.base_url
+    route = request.path
     if not is_behind_auth(route):
         return True
     token = extract_token(request)
     if token is None:
+        logger.info("%s - No token", route)
         return False
+    
     if token in TOKENS:
+        logger.info("%s - Valid token: %s", route, token)
         return True
+    
     read_tokens() # to account for tokens added after server started
-    return token in TOKENS
+    if token in TOKENS:
+        logger.info("%s - Valid token: %s", route, token)
+        return True
+    logger.info("%s - Invalid token: %s", route, token)
+    return False
 
 def extract_token(req):
     method = req.method
     if method == 'GET':
         return req.args.to_dict().get('token')
-    elif method == 'POST' and req.json:
+    if method == 'POST' and req.json:
         return req.json.get('token')
-    else:
-        return None
+    return None
 
 def is_behind_auth(route):
     if route.endswith(('.css', '.js', '.ico')) \
