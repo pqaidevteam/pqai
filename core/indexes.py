6d09519925f00ea6545854b1bf291cf17432f8e5
Mahesh Maan <mahesh.maan@greyb.com>
2024-04-14 19:38:46 +0530

    Load indexes alphabetically

--- core/indexes.py
+++ core/indexes.py
@@ -185,10 +185,12 @@ class IndexesDirectory():
 
     def get(self, index_id):
         if index_id == "*" or index_id == "all":
-            return [self._get_one_index(idx) for idx in self.available()]
-        
-        index_ids = filter(lambda x: x.startswith(index_id), self.available())
-        indexes = [self._get_one_index(idx) for idx in index_ids]
+            index_ids = self.available()
+        else:
+            index_ids = filter(lambda x: x.startswith(index_id), self.available())
+        indexes = []
+        for idx in sorted(index_ids):
+            indexes.append(self._get_one_index(idx))
         return indexes
 
     def _get_one_index(self, index_id):
